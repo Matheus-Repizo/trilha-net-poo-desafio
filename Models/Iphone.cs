d0c0a1246efcda53b7d42c022719d76a3990499e
Matheus Repizo <matheusdprepizo@gmail.com>
2024-11-29 16:40:23 -0300

    Feat: Implementando o Todo da Classe Iphone.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Você instalou o {nomeApp} através do AppStore com Sucesso!");
+        }
     }
 }
