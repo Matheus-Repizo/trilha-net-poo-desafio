a6e50906f8b105013a4a3563f8fddb42db089d00
Matheus Repizo <matheusdprepizo@gmail.com>
2024-11-29 16:37:42 -0300

    Feat: Implementando o Todo da Classe Nokia.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -2,9 +2,13 @@ namespace DesafioPOO.Models
 
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
-       
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Você instalou o Aplicativo {nomeApp} com sucesso!");
+        }
+
     }
 }
